241d1bfe6b0f79cf489d245ad22a75ba30bf7305
aimilios <aimilios@users.noreply.github.com>
2018-09-29 22:32:03 +0300

    More Sources Signals and VI Formulas

--- RCSeries_Calc.m
+++ RCSeries_Calc.m
@@ -1,33 +1,38 @@
 clear all;
 clc;
 
-syms s t R C A w Uo
+syms s t R C A w Uo V
 
-vs(t) = A*cos(w*t);
-Vs(s) = laplace(vs(t));
+vs(t) = V*heaviside(t); %Direct Voltage Source
+vs(t) = V*cos(w*t);     %Alternating Voltage Source
+%vs(t) = V*(heaviside(t)-heaviside(t-0.001)); %Pulse Source
+%vs(t) = V*dirac(t);     %Impulse Signal
+Vs(s) = laplace(vs(t),t,s);
 
 I(s) = (1/R)*(s*Vs(s)-Uo)*(1/(s+1/(R*C)));
+Vc(s) = (1/(s+1/(R*C)))*(Vs(s)/(R*C)+Uo);
 
 i(t) = ilaplace(I(s));
+vc(t) = ilaplace(Vc(s));
 
-Av = 28.2842;
+%Capacitor Formula:
+vc_2 = (1/C)*int(i(t),0,t)+Uo;
+vr_2 = R*i(t);
+
+Vv = 28.2842;
 Rv = 10;
 Cv = 0.0001;
-wv = 2*pi*50;
+wv = 2*pi*500;
 Uov = 0;
 
-i1(t) = subs(i,[A,R,C,w,Uo],[Av,Rv,Cv,wv,Uov]);
+i1(t) = subs(i,[V,R,C,w,Uo],[Vv,Rv,Cv,wv,Uov]);
+vc1(t) = subs(vc,[V,R,C,w,Uo],[Vv,Rv,Cv,wv,Uov]);
 
 disp('Transform Completed');
 
-
-j=1;
-for tv = 0:0.001:0.1
-    a(j) = vpa(i1(tv));
-    j = j + 1;
-end
-plot(a)
-
+i
+vc
+vc_2
 
 
 
